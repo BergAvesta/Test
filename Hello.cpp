3a15f4a3ebb122584b31265ba7f67249821632ec
BergAversta <kevin791031123@yahoo.com.tw>
2015-03-11 12:01:09 +0800

    second commit

--- Hello.cpp
+++ Hello.cpp
@@ -1,1 +1,17 @@
-jifejifij
+#include <stdio.h>
+
+int main()
+
+{
+	char H[50]="HelloWorld!" ;
+	printf("H=%s \n",H);
+	printf("H=%d \n",H);
+	for(int i=0;i<20;i++)
+	{
+	printf("H=%c \n",H[2*i]);
+	}
+	printf("H=%u \n",H);
+	printf("H=%f \n",H);
+	return 0;
+}
+
